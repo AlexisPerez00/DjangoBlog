abc7123c6717e3b430d6b61c8164ba3773ca5e41
Alexis Perez <alexisperez00.cappi@gmail.com>
2022-09-29 22:23:03 -0500

    App semiterminada

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import Error404 from "containers/errors/Error404";
+import Home from "containers/pages/Home";
+import store from "./store";
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import BlogPost from "containers/pages/blog/BlogPost";
+import Blog from "containers/pages/blog/Blog";
+import BlogCategory from "containers/pages/blog/category/BlogCategory";
+import Search from "containers/pages/Search";
+import FullWidthLayout from "hocs/layouts/FullWidthLayout";
+import AllCategories from "components/blog/AllCategories";
+import ErrorInProgress from "containers/errors/ErrorInProgress";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path="*" element={<Error404/>}/>
+          <Route path="/about/" element={<ErrorInProgress/>}/>
+          <Route path="/contact/" element={<ErrorInProgress/>}/>
+
+          <Route path="/" element={<Home/>}/>
+
+          <Route path="/blog" element={<Blog/>}/>
+          <Route path="/blog/post/:slug" element={<BlogPost/>}/>
+          <Route path="/blog/categories/" element={<FullWidthLayout><AllCategories/></FullWidthLayout>}/>
+          <Route path="/blog/categories/:category_id" element={<BlogCategory/>}/>
+          
+          <Route path="/search/:term" element={<Search/>}/>
+
+
+
+        </Routes>
+      </Router>
+    </Provider>
   );
 }
 
---
+++ src/containers/errors/Error404.jsx
@@ -0,0 +1,18 @@
+import FullWidthLayout from "hocs/layouts/FullWidthLayout";
+
+function Error404(){
+    return(
+        <FullWidthLayout>
+            <div class="bg-white py-12">
+                <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                    <div class="lg:text-center">
+                        <h2 class="text-lg font-semibold text-indigo-600">Oops</h2>
+                        <p class="mt-2 text-3xl font-bold leading-8 tracking-tight text-gray-900 sm:text-4xl">Sorry, this page was not found</p>
+                        <p class="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">Lorem ipsum dolor sit amet consect adipisicing elit. Possimus magnam voluptatum cupiditate veritatis in accusamus quisquam.</p>
+                    </div>
+                </div>
+            </div>        </FullWidthLayout>
+    )
+}
+
+export default Error404
